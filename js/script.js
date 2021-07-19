45999d4f81eb82eb297e70401cc222bfc956c461
mirkone1994 <mirkochiappori@gmail.com>
2021-07-19 16:07:27 +0200

    push for hotwell

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
 /*Descrizione:
 ES 1
 Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
-Stampare a schermo (in pagina) attraverso un ciclo for-in tutte le proprietà dell'oggetto.
-ES 2
-Creare un array di oggetti di studenti.
-Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
-Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.*/
+Stampare a schermo (in pagina) attraverso un ciclo for-in tutte le proprietà dell'oggetto.*/
+var studentElement = document.getElementById("student");
 var student = {
     name: "Mirko",
     surname: "Chappori",
@@ -14,3 +11,4 @@ var student = {
 for ( var key in student) {
     console.log(student[key]);
 }
+
---
+++ js/script2.js
@@ -0,0 +1,27 @@
+/*ES 2
+Creare un array di oggetti studenti.
+Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.*/
+var objectStudents = [
+    {
+        names: "Mirko",
+        surname: "Chiappori",
+        age: 27,
+    },
+    {
+        names: "Lorenzo",
+        surname: "Cazzola",
+        age: 26,
+    },
+    {
+        names: "Giovanni",
+        surname: "Leoncini",
+        age: 27,
+    },
+];
+console.table(objectStudents);
+var names = prompt("Nome?");
+var surname = prompt("Cognome?");
+var age = parseInt(prompt("Età?"));
+objectStudents.names = names;
+console.table(objectStudents);
